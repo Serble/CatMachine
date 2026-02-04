4523d5f97eaa7e347b65c7a2c04fa9a60912e79b
CoPokBl <copokbl@serble.net>
2026-02-04 20:34:37 +1100

    add run script for snake

---
+++ ExampleProjects/Snake/run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Navigate to the script's directory
+cd "$(dirname "$0")"
+
+./build.sh
+
+# Keep in mind you still need to pass CLI args you want
+# passed to the VM, for example to enable a display backend
+# you might do:
+# --renderer raylib
+
+echo "Running..."
+dotnet run --project ../../CatVM/CatVM.csproj ./bin/snake.bin --renderer raylib $*
+status=$?
+echo "Application exited with status code $status"
