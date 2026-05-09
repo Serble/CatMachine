b1c1999aea581d13ae99cb6a7814619d89d671e4
calcilore <github@calcilore.net>
2026-05-10 04:07:22 +1000

    Made the disk write data to real disk

--- CatVM/Extensions/Disk.cs
+++ CatVM/Extensions/Disk.cs
@@ -1,16 +1,72 @@
 using System.Diagnostics;
+using System.Threading.Channels;
 using CatVM.Serial;
 
 namespace CatVM.Extensions;
 
-public class Disk(Stream stream, long picosPerBlock, int queueCapacity = 32) : CommandBasedSerialDevice<Disk.Mode>, IDisposable, IAsyncDisposable {
+// TODO: Disk bounds checking
+public class Disk : CommandBasedSerialDevice<Disk.Mode> {
     public override uint Type => 0x02;
 
     private const long BlockSize = 512;
 
-    private Dictionary<uint, byte[]> _unwrittenData = [];
-    private Queue<(bool isRead, uint memAddr, uint startBlock, uint blockCount)> _queue = new(queueCapacity);
+    private Lock _lock = new();
+    private Lock _streamLock = new();
+    private Dictionary<uint, WriteCached> _unwritten = new();
+    private Channel<WriteCached> _channel = Channel.CreateUnbounded<WriteCached>();
+    
+    private Queue<(bool isRead, uint memAddr, uint startBlock, uint blockCount)> _queue;
     private bool _isRunning;
+    private readonly Stream _stream;
+    private readonly long _picosPerBlock;
+    
+    public Disk(Stream stream, long picosPerBlock, int queueCapacity = 32, CancellationToken? token = null) {
+        _stream = stream;
+        _picosPerBlock = picosPerBlock;
+        _queue = new Queue<(bool isRead, uint memAddr, uint startBlock, uint blockCount)>(queueCapacity);
+        
+        Task.Run(() => Run(token ?? CancellationToken.None));
+    }
+
+    // TODO: ask if this is how it should stop, and if it should catch exceptions
+    private async void Run(CancellationToken token) {
+        List<WriteCached> written = [];
+        
+        while (!token.IsCancellationRequested) {
+            try {
+                await _channel.Reader.WaitToReadAsync(token);
+            }
+            catch (OperationCanceledException) {
+                break;
+            }
+            
+            while (_channel.Reader.TryRead(out WriteCached? toWrite)) {
+                written.Add(toWrite);
+                lock (_streamLock) {
+                    _stream.Seek(toWrite.Block * BlockSize, SeekOrigin.Begin);
+                    _stream.Write(toWrite.Data);
+                }
+            }
+
+            if (written.Count > 0) {
+                lock (_streamLock) {
+                    _stream.Flush();
+                }
+
+                lock (_lock) {
+                    foreach (WriteCached wrote in written) {
+                        if (_unwritten.TryGetValue(wrote.Block, out WriteCached? cache) && cache.WriteId == wrote.WriteId) {
+                            _unwritten.Remove(wrote.Block);
+                        }
+                    }
+                }
+                
+                written.Clear();
+            }
+            
+            Thread.Sleep(100);
+        }
+    }
 
     protected override int GetArgCount(Mode mode) {
         return mode switch {
@@ -53,31 +109,40 @@ public class Disk(Stream stream, long picosPerBlock, int queueCapacity = 32) : C
         void Execute() {
             Stopwatch sw = Stopwatch.StartNew();
             if (isRead) {
-                for (uint block = startBlock; block < startBlock + blockCount; block++) {
+                for (uint block = 0; block < blockCount; block++) {
                     int startAddr = (int)(memAddr + block * BlockSize);
                     Span<byte> span = vm.Memory.AsSpan(startAddr..(startAddr + (int)BlockSize));
+
+                    uint absBlock = startBlock + block;
+                    WriteCached? data;
+                    lock (_lock) {
+                        _unwritten.TryGetValue(absBlock, out data);
+                    }
                     
-                    if (_unwrittenData.TryGetValue(block, out byte[]? data)) {
-                        data.CopyTo(span);
+                    if (data != null) {
+                        data.Data.CopyTo(span);
                     }
                     else {
-                        stream.Seek(block * BlockSize, SeekOrigin.Begin);
-                        stream.ReadExactly(span);
+                        lock (_streamLock) {
+                            _stream.Seek(absBlock * BlockSize, SeekOrigin.Begin);
+                            _stream.ReadExactly(span);
+                        }
                     }
                 }
             }
             else {
-                for (uint block = startBlock; block < startBlock + blockCount; block++) {
+                for (uint block = 0; block < blockCount; block++) {
                     int startAddr = (int)(memAddr + block * BlockSize);
                     Span<byte> span = vm.Memory.AsSpan(startAddr..(int)(startAddr + BlockSize));
 
-                    if (_unwrittenData.TryGetValue(block, out byte[]? array)) {
-                        span.CopyTo(array);
-                    }
-                    else {
-                        array = new byte[BlockSize];
-                        span.CopyTo(array);
-                        _unwrittenData[block] = array;
+                    uint absBlock = startBlock + block;
+                    byte[] array = new byte[BlockSize];
+                    span.CopyTo(array);
+                    
+                    lock (_lock) {
+                        WriteCached cache = new(absBlock, array);
+                        _unwritten[absBlock] = cache;
+                        _channel.Writer.TryWrite(cache); // should never fail
                     }
                 }
             }
@@ -88,11 +153,11 @@ public class Disk(Stream stream, long picosPerBlock, int queueCapacity = 32) : C
             ExecuteOperation(vm);
         }
 
-        if (picosPerBlock == 0) {
+        if (_picosPerBlock == 0) {
             Execute();
         }
         else {
-            vm.RunIn(blockCount * picosPerBlock, Execute);
+            vm.RunIn(blockCount * _picosPerBlock, Execute);
         }
     }
     
@@ -100,16 +165,10 @@ public class Disk(Stream stream, long picosPerBlock, int queueCapacity = 32) : C
         Read  = 1,  // args: Mem Addr, Start block, Block Count
         Write = 2,  // args: Mem Addr, Start block, Block Count
     }
-
-    public void Dispose() {
-        stream.Flush();
-        stream.Dispose();
-        GC.SuppressFinalize(this);
-    }
-
-    public async ValueTask DisposeAsync() {
-        await stream.FlushAsync();
-        await stream.DisposeAsync();
-        GC.SuppressFinalize(this);
+    
+    private class WriteCached(uint block, byte[] data) {
+        public readonly uint Block = block;
+        public readonly byte[] Data = data;
+        public readonly Guid WriteId = Guid.NewGuid();
     }
 }
