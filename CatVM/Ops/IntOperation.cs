6615a7d8720d9bd44af05792dfbd04e91f4526c4
copokbl <copokbl@serble.net>
2026-05-10 21:49:39 +1000

    fix prived instructions

--- CatVM/Ops/IntOperation.cs
+++ CatVM/Ops/IntOperation.cs
@@ -3,21 +3,23 @@ namespace CatVM.Ops;
 public static class IntOperation {
     
     public static void IntR(CatVM vm) {
+        byte idReg = vm.Read8();
+        
         if (!vm.TryPrivileged()) {
             return;
         }
         
-        byte idReg = vm.Read8();
         byte intNumber = (byte)(vm.Cpu.Get(idReg) & 0xFF);
         vm.Interrupt(intNumber);
     }
     
     public static void IntI(CatVM vm) {
+        byte intNumber = vm.Read8();
+        
         if (!vm.TryPrivileged()) {
             return;
         }
         
-        byte intNumber = vm.Read8();
         vm.Interrupt(intNumber);
     }
     
--- CatVM/Ops/SerialOperation.cs
+++ CatVM/Ops/SerialOperation.cs
@@ -6,12 +6,22 @@ public static class SerialOperation {
         byte destReg = vm.Read8();
         byte portReg = vm.Read8();
         uint port = vm.Cpu.Get(portReg);
+        
+        if (!vm.TryPrivileged()) {
+            return;
+        }
+        
         vm.Cpu.Set(destReg, vm.GetSerialDevice(port).Input(vm));
     }
     
     public static void InRI(CatVM vm) {
         byte destReg = vm.Read8();
         uint port = vm.ReadWord();
+        
+        if (!vm.TryPrivileged()) {
+            return;
+        }
+        
         vm.Cpu.Set(destReg, vm.GetSerialDevice(port).Input(vm));
     }
     
@@ -20,6 +30,11 @@ public static class SerialOperation {
         byte dataReg = vm.Read8();
         uint port = vm.Cpu.Get(portReg);
         uint data = vm.Cpu.Get(dataReg);
+        
+        if (!vm.TryPrivileged()) {
+            return;
+        }
+        
         vm.GetSerialDevice(port).Output(vm, data);
     }
     
@@ -27,6 +42,11 @@ public static class SerialOperation {
         byte portReg = vm.Read8();
         uint port = vm.Cpu.Get(portReg);
         uint data = vm.ReadWord();
+        
+        if (!vm.TryPrivileged()) {
+            return;
+        }
+        
         vm.GetSerialDevice(port).Output(vm, data);
     }
     
@@ -34,12 +54,22 @@ public static class SerialOperation {
         uint port = vm.ReadWord();
         byte dataReg = vm.Read8();
         uint data = vm.Cpu.Get(dataReg);
+        
+        if (!vm.TryPrivileged()) {
+            return;
+        }
+        
         vm.GetSerialDevice(port).Output(vm, data);
     }
     
     public static void OutII(CatVM vm) {
         uint port = vm.ReadWord();
         uint data = vm.ReadWord();
+        
+        if (!vm.TryPrivileged()) {
+            return;
+        }
+        
         vm.GetSerialDevice(port).Output(vm, data);
     }
 }
