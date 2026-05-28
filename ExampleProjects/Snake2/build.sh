59feea09fb211771f9ab740a989f54a8bf52953e
calcilore <github@calcilore.net>
2026-05-29 03:13:54 +1000

    Added main menu to snake 2

--- ExampleProjects/Snake2/build.sh
+++ ExampleProjects/Snake2/build.sh
@@ -8,12 +8,14 @@ rm -rf data
 mkdir -p data
 
 echo "Building images"
-for f in ./images/*.png; do
+for f in ./images/snake_*.png ./images/apple.png; do
     # remove extension and folder path
     x="${f##*/}"
     x="${x%.*}"
     python ../tools/image_to_tiles.py -i $f -p palette0.palette -o ./data/$x
 done
+
+python ../tools/image_to_buffer.py -i ./images/title_screen.png -o ./data/title_screen
 
 ../tools/build.sh $MAIN_FILE
 exit $?
